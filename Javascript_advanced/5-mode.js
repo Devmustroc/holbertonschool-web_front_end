010d3d1ec852ce5b6b8cd9fd8b80daba279ff999
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-04-28 18:40:57 +0200

    JavaScript Advanced

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -7,3 +7,29 @@ function changeMode(size, weight, transform, background, color) {
         document.body.style.color = color;
     }
 }
+function main() {
+    const paragraph = document.createElement("p");
+    const text = document.createTextNode("Welcome Holberton!");
+    paragraph.appendChild(text);
+    document.body.appendChild(paragraph);
+
+    const spookyButton = document.createElement("button");
+    const spookyText = document.createTextNode("Spooky");
+    spookyButton.appendChild(spookyText);
+    document.body.appendChild(spookyButton);
+
+    const darkButton = document.createElement("button");
+    const darkText = document.createTextNode("Dark mode");
+    darkButton.appendChild(darkText);
+    document.body.appendChild(darkButton);
+
+    const screamButton = document.createElement("button");
+    const screamText = document.createTextNode("Scream mode");
+    screamButton.appendChild(screamText);
+    document.body.appendChild(screamButton);
+
+    spookyButton.addEventListener("click", changeMode(9, "bold", "uppercase", "pink", "green"));
+    darkButton.addEventListener("click", changeMode(12, "bold", "capitalize", "black", "white"));
+    screamButton.addEventListener("click", changeMode(12, "normal", "lowercase", "white", "black"));
+}
+main();
