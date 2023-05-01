20f2aae8cc923719c2ba7cc6f08d079ef7d16786
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-05-01 13:26:05 +0200

    JavaScript Advanced

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,18 @@
+const createElement = (data) => {
+    const element = document.createElement("div");
+    element.innerHTML = data;
+    return element;
+};
+const queryWikipedia = (callback) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*')
+    xhr.responseType = "json";
+    xhr.send();
+    xhr.onload = () => {
+        callback(xhr.response.query.pages["21721040"].extract)
+    }
+    xhr.onerror = () => {
+        alert("Request failed");
+    };
+}
+queryWikipedia(createElement);
